53a3126eea075e6d4e8b29ca633b8f1f45124794
andr_l95 <andreylyssenko95@gmail.com>
2022-03-29 22:21:36 +0600

    Подключил контроль эффектов

--- js/editor-control-scale.js
+++ js/editor-control-scale.js
@@ -1,1 +1,29 @@
 // Изменение масштаба загружаемого изображения (ТЗ 2.1)
+
+const scaleValue = document.querySelector('.scale__control--value');
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const imgPreview = document.querySelector('.img-upload__preview').querySelector('img');
+const SCALE_STEP = 25;
+const MIN_SCALE = 0;
+const MAX_SCALE = 100;
+
+let DEFAULT_SCALE = 100;
+
+scaleSmaller.addEventListener('click', () => {
+  if(DEFAULT_SCALE > MIN_SCALE) {
+    DEFAULT_SCALE -= SCALE_STEP;
+    imgPreview.style.transform = `scale(${DEFAULT_SCALE / 100})`;
+    scaleValue.value = `${DEFAULT_SCALE}%`;
+  }
+});
+
+scaleBigger.addEventListener('click', () => {
+  if(DEFAULT_SCALE < MAX_SCALE) {
+    DEFAULT_SCALE += SCALE_STEP;
+    imgPreview.style.transform = `scale(${DEFAULT_SCALE / 100})`;
+    scaleValue.value = `${DEFAULT_SCALE}%`;
+  }
+});
+
+export {  imgPreview  };
